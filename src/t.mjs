3804e9978b29eb446a489b4a4b7878c7ab1e80a1
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-07-20 16:00:58 -0500

    Using functions to for type proofing rather than objects

--- src/t.mjs
+++ src/t.mjs
@@ -1,26 +1,17 @@
 let _types = new Map();
 
 const typedef = (type, def) => {
-  if(typeof(type) == "string" && typeof(def) == "object"){
-    if(_types.has(type)){
-      return undefined;
-    }else{
-      const r = {type: type, def: Object.keys(def).toString()};
-      _types.set(r.type, r.def);
-      return r;
-    }
-  }else{
-    return undefined;
+  if(typeof(type) == 'string' && typeof(def) == 'function' && !_types.has(type)){
+    _types.set(type, def);
+    return true;
   }
+  return false;
 }
 
 const isType = (type, value) => {
-  if(typeof(type) == "string" && typeof(value) == "object"){
-    if(_types.has(type)){
-      return _types.get(type) == Object.keys(value).toString();
-    }else{
-      return undefined;
-    }
+  if(typeof(type) == "string" && _types.has(type)){
+    const proof = _types.get(type);
+    return proof(value);
   }else{
     return undefined;
   }
@@ -30,18 +21,5 @@ const val = (type, value) => {
   const evaluate = isType(type, value);
   return evaluate === true ? value : evaluate;
 }
-// const T = (obj) => { return Object.keys(obj).toString(); }
-//
-// const typesEq = (a , b) => { return T(a) == T(b); }
-//
-// const isString = (a) => { return typeof(a) === "string"; }
-//
-// const isNum = (a) => { return typeof(a) === "number"; }
-//
-// const isObj = (a) => { return typeof(a) === "obj"; }
-//
-// const ref = (t, v) => {
-//   return typeof(t) === "string" && typeof(v) === "object" ? {t: t,val: v} : undefined;
-// }
 
 export {typedef, isType, val};
--- src/test.mjs
+++ src/test.mjs
@@ -5,44 +5,54 @@ typedef(...) Tests
 */
 console.assert(typedef !== undefined);
 
-console.assert(typedef("Person", {
-  name: '',
-  age: 0,
-  address: {},
-}) !== undefined);
+console.assert(typedef('u8', (x) => {
+  return typeof(x) == 'number' && x > -1 && x < 256;
+}));
 
-console.assert(typedef("Person", {}) === undefined);
+console.assert(!typedef('u8', (x) => {return true}));
 
-console.assert(typedef("number", 0) === undefined);
+console.assert(!typedef('u32', {}));
 
-console.assert(typedef("string", 'Alice') === undefined);
+console.assert(!typedef('number', 1));
+
+console.assert(typedef('i8', (x) => {
+  return typeof(x) == 'number' && x > -128 && x < 128;
+}));
+
+console.assert(typedef('number', (x) => {
+  return typeof(x) == 'number';
+}));
+
+console.assert(typedef('Rectangle', (x) => {
+  return typeof(x) == 'object' &&
+    typeof(x.length) == 'number' &&
+    typeof(x.width) == 'number';
+}));
 
 /*
 isType(...) Tests
 */
 console.assert(isType !== undefined);
 
-console.assert(isType('Person', {
-  name: 'Alice',
-  age: 25,
-  address: {},
-}));
+console.assert(isType('number', 1));
 
-console.assert(isType('Person', {
-  length: 2,
-  width: 3,
-}) === false);
+console.assert(isType('u32', 2) === undefined);
 
-console.assert(isType('Rectangle', {
-  name: 'Alice',
-  age: 25,
-  address: {},
-}) === undefined);
+console.assert(isType('u8', -1) === false);
+
+console.assert(isType('Rectangle', {length: 1, width: 2}));
 
 /*
 val(...) Tests
 */
 console.assert(val !== undefined);
+
+console.assert(typedef('Person', (p) => {
+  return typeof(p) == 'object' &&
+    typeof(p.name) == 'string' &&
+    typeof(p.age) == 'number' &&
+    typeof(p.address) == 'object';
+}));
 
 let alice = {
   name: 'Alice',
@@ -54,4 +64,4 @@ console.assert(val('Person', alice) === alice);
 
 console.assert(val('Person', alice) !== undefined);
 
-console.assert(val('Rectangle', alice) === undefined);
+console.assert(val('Planet', alice) === undefined);
