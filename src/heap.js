7883dffe1c832b8de28f3b1037a676c093f07b0b
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-07-27 14:30:13 -0500

    Using builder pattern / singleton for modules

--- src/heap.js
+++ src/heap.js
@@ -1,16 +1,30 @@
-function Heap(types) {
-    // heap constructor
-    //  Takes all the types and maps them to arrays
-    this.types = new Map();
-    for(let i = 0; i < types.len; i++){
-        types.add(types.get(i), []);
+// The single heap instance
+let _heap = undefined;
+
+// The private heap module
+const __HEAP__ = {
+    def: (type, variable) => {
+
+    },
+    undef: (variable) => {
+
+    },
+};
+
+// The public heap module that will we exported
+const Heap = {
+    // Constructor that returns the private heap
+    //  This is an implementation of the builder pattern that treats
+    //  _heap as a singleton
+    from_types: (types) => {
+        if(_heap === undefined){
+            _heap = new Map();
+            for(let i = 0; i < types.len(); i++){
+                _heap.set(types.get(i), []);
+            }
+            return __HEAP__;
+        }else{
+            return undefined;
+        }
     }
-
-    this.def = (type, variable) => {
-
-    };
-
-    this.undef = (variable) => {
-
-    };
-}
+};
